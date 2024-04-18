fb6b03ab0a987580f496981e16036e0ad4b9b83c
dylanjmolnar <dylanjosephmolnar@gmail.com>
2024-04-17 22:55:56 -0400

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media.licdn.com",
-      },
-    ],
-  },
-};
+  output: "export",
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+// export default nextConfig;
