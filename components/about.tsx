b7296eb7232de3e1137adce1a9e9338514a7734b
dylanjmolnar <dylanjosephmolnar@gmail.com>
2024-04-19 01:21:03 -0400

    updates

--- components/about.tsx
+++ components/about.tsx
@@ -22,20 +22,8 @@ export default function About() {
         After graduating with a degree in{" "}
         <span className="font-medium">Chemical Engineering</span>, I decided to pursue my
         passion for programming.
-        <span className="italic"> My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        challenges that arise from problem solving. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and Vite
-        </span>
-        . I am also familiar with TypeScript and Python. I am always looking to
-        learn new languages and expand my portfolio.
-      </p>
-
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy 
-        coding my own projects & working on cars and bikes. I'm always looking to{" "}
+        <span className="italic">When I'm not coding</span>, I'm practicing Spanish or 
+        working on cars and bikes. I'm always looking to{" "}
         <span className="font-medium">learning new things</span>.
       </p>
     </motion.section>
