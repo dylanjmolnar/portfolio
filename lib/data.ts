5d8c4e692721433268e9f2bb4eb2c4479dfb9a64
dylanjmolnar <dylanjosephmolnar@gmail.com>
2024-04-21 19:49:15 -0400

    update

--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import rmtdevImg  from "@/public/automodser.png";
+import rmtdevImg  from "@/public/rmtdev.png";
 
 export const links = [
   {
