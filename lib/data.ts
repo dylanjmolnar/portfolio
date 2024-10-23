a704b4072b7f48d2e7168a7ff4e9043fc429c52d
Dylan Molnar <dylanjosephmolnar@gmail.com>
2024-10-23 16:19:37 -0400

    updates

--- lib/data.ts
+++ lib/data.ts
@@ -3,7 +3,7 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import autoModSerImg  from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+//import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
@@ -72,7 +72,7 @@ export const projectsData = [
     description:
       "Helped develop and maintain the website for Retail Smart to help automate client-supplier relation.",
     tags: ["TypeScript", "Chakra UI", "React", "Firebase"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: autoModSerImg,
   },
 ] as const;
 
