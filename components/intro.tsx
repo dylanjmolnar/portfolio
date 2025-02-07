5ca44e27ae5cf178273ab692d37ecea28b25ffb0
Dylan Molnar <dylanjosephmolnar@gmail.com>
2025-02-07 00:07:12 -0500

    Fix photo

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://media.licdn.com/dms/image/v2/D4E03AQG81Xi_ukqokQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1702348370933?e=1730332800&v=beta&t=qgmMjkdlwFnZeweKYMo_372o_zQ6W0Q6_a3SvqsnmUk"
+              src="https://media.licdn.com/dms/image/v2/D4E03AQG81Xi_ukqokQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1702348370933?e=1744243200&v=beta&t=T1aPhjxoO821d--nIRPlRD1zrtsglP0XXIFNOkj2UYI"
               alt="Dylan Portrait"
               width="192"
               height="192"
