12cd5d2d72399a852ab28e97872b4c8b5dc770fc
Dylan Molnar <dylanjosephmolnar@gmail.com>
2025-04-19 12:35:37 -0400

    updates

--- lib/data.ts
+++ lib/data.ts
@@ -2,8 +2,8 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import autoModSerImg  from "/rmtdev.png";
-import RIQImg  from "/wordanalytics.png";
+import autoModSerImg  from "rmtdev.png";
+import RIQImg  from "wordanalytics.png";
 // import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
