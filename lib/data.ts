6812afbee6dfd1d47ed9540d7a6362275f0b6433
Dylan Molnar <dylanjosephmolnar@gmail.com>
2025-04-19 14:37:07 -0400

    odd error

--- lib/data.ts
+++ lib/data.ts
@@ -70,7 +70,7 @@ export const projectsData = [
     description:
       "Helped develop and maintain the website for Retail Smart to help automate client-supplier relation.",
     tags: ["TypeScript", "Chakra UI", "React", "Firebase"],
-    imageUrl: "wordanalytics.png",
+    imageUrl: "wordanalytics.PNG",
   },
 ] as const;
 
